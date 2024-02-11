520df4fa78d5687e426068800c0fc01497039fcf
Patrick Njiru <patricknjiru93@gmail.com>
2024-02-11 22:03:55 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// import React, { useState } from 'react'
+// import { data } from '../data'
+
+function App() {
+
+  return (
+    <div className="">
+      App
+    </div>
+  )
+}
+
+export default App
